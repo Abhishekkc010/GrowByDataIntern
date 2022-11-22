e21fd9d2a83079b727b7c61511955b5a64d7d152
Abhishekkc010 <abhishekkc010@gmail.com>
2022-11-22 11:47:35 +0545

    2nd

--- tutorial/game.py
+++ tutorial/game.py
@@ -14,28 +14,31 @@ if answer.lower() == "central processing unit":
     print('Correct!')
     score += 1
 else:
-    print("Incorrect!")
-
+    print("Incorrect! You Lost")
+    quit()
 answer = input("What does GPU stand for? ")
 if answer.lower() == "graphics processing unit":
     print('Correct!')
     score += 1
 else:
-    print("Incorrect!")
+    print("Incorrect! You Lost")
+    quit()
 
 answer = input("What does RAM stand for? ")
 if answer.lower() == "random access memory":
     print('Correct!')
     score += 1
 else:
-    print("Incorrect!")
+    print("Incorrect! You Lost")
+    quit()
 
 answer = input("What does PSU stand for? ")
 if answer.lower() == "power supply":
     print('Correct!')
+     
     score += 1
 else:
-    print("Incorrect!")
-
+    print("Incorrect! You Lost")
+    quit()
 print("You got " + str(score) + " questions correct!")
 print("You got " + str((score / 4) * 100) + "%.")
